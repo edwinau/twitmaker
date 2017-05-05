9c211ffd9839930a2a4e9217f57b3e45f4f9cac2
EDWIN AU <edwinau@rogers.com>
2017-05-04 21:23:56 -0400

    json and html respsonse working

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,24 +1,24 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$(document).ready(function(){
-  $('#new_tweet').on('submit', function(e){
-    e.preventDefault();
-
-    $.ajax({
-      url: $(this).attr('action'),
-      method: $(this).attr('method'),
-      data: $(this).serialize(),
-      dataType: 'html'
-    }).done(function(response){
-      for (var i = 0; i<response.length; i++){
-        var li = '<li>' + response + '<li>'(response(i).tweet);
-        $('ul').append(li);
-      };
-      console.log(response);
-    });
-  });
-});
+// $(document).ready(function(){
+//   $('#new_tweet').on('submit', function(e){
+//     e.preventDefault();
+//
+//     $.ajax({
+//       url: $(this).attr('action'),
+//       method: $(this).attr('method'),
+//       data: $(this).serialize(),
+//       dataType: 'html'
+//     }).done(function(response){
+//       for (var i = 0; i<response.length; i++){
+//         var li = '<li>' + response + '<li>'(response(i).tweet);
+//         $('ul').append(li);
+//       };
+//       console.log(response);
+//     });
+//   });
+// });
 
 
 $(document).ready(function(){
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,13 +6,25 @@ class TweetsController < ApplicationController
 
   def create
     @tweet = Tweet.new(tweet_params)
+      respond_to do |format|
+        format.html do
+          if @tweet.save
+            redirect_to tweets_path
+          else
+            render :index
+          end
+        end
+      end
 
-    if @tweet.save
-      redirect_to tweets_path
-    else
-      render :index
+      format.json do
+        if @tweet.save
+          render json: @tweet
+        else
+          render :index
+        end
+      end
     end
-  end
+  
 
   def destroy
   end
