b298850f527104d8e488012ab51ca9c1a1b3a232
EDWIN AU <edwinau@rogers.com>
2017-05-04 21:07:56 -0400

    replace data with response

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -9,10 +9,30 @@ $(document).ready(function(){
       url: $(this).attr('action'),
       method: $(this).attr('method'),
       data: $(this).serialize(),
+      dataType: 'html'
+    }).done(function(response){
+      for (var i = 0; i<response.length; i++){
+        var li = '<li>' + response + '<li>'(response(i).tweet);
+        $('ul').append(li);
+      };
+      console.log(response);
+    });
+  });
+});
+
+
+$(document).ready(function(){
+  $('#new_tweet').on('submit', function(e){
+    e.preventDefault();
+
+    $.ajax({
+      url: $(this).attr('action'),
+      method: $(this).attr('method'),
+      data: $(this).serialize(),
       dataType: 'json'
     }).done(function(response){
       for (var i = 0; i<response.length; i++){
-        var li = '<li>' + data + '<li>'(response(i).tweet);
+        var li = '<li>' + response + '<li>'(response(i).tweet);
         $('ul').append(li);
       };
       console.log(response);
